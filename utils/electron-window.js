a0e7bd46577bddf8d668cd8aa113cdf60a49fbf9
chen <92xiaodao@gmail.com>
2024-09-12 23:52:29 +0800

    feat: update

--- utils/electron-window.js
+++ utils/electron-window.js
@@ -33,6 +33,22 @@ class ElectronWindow {
     const win = new BrowserWindow({
       width: 1000,
       height: 500,
+
+      // maxWidth: 1000, // 窗口最大宽度
+      // minWidth: 1000, // 窗口最小宽度
+      // maxHeight: 500, // 窗口最大高度
+      // minHeight: 500, // 窗口最小高度
+
+      // resizable: true, // 窗口是否可改变大小
+      // movable: true, // 窗口是否可移动
+
+      // x: 1000, // 窗口距离左边的距离
+      // y: 1000, // 窗口距离上边的距离
+
+      // icon: path.join(__dirname, 'log.ico'),
+
+      // frame: true, // 控制 标题栏, 菜单栏, 边框 是否展示
+
       webPreferences: {
         nodeIntegration: true, // 允许在渲染进程（在窗口）里面使用 node.js
         contextIsolation: false, // 关闭上下文隔离
@@ -49,6 +65,16 @@ class ElectronWindow {
 
     return win;
   };
+
+  /**
+   * 标题优先级
+   *
+   * 1. HTML.title
+   * 2. BrowserWindow.title
+   * 3. package.json.name
+   * 4. Electron 默认值: Electron
+   *
+   */
 }
 
 module.exports = {
